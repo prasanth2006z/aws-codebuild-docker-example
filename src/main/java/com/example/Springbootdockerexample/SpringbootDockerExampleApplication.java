a5311ce7223c6fd95f6dc6823690998951886969
root <root@ip-10-0-0-65.ap-south-1.compute.internal>
2019-07-25 10:00:24 +0000

    src'

---
+++ src/main/java/com/example/Springbootdockerexample/SpringbootDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbootdockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerExampleApplication.class, args);
+	}
+
+}
